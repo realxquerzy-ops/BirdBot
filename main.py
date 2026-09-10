d84411b21d828a6ed9a0bf2073fc5bd0b45ed7a0
xquerzy <realxquerzy@gmail.com>
2026-09-10 19:49:35 +0300

    fix: enable members intent for reliable presence status

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ RESOURCE_GUILD_IDS = [
 intents = discord.Intents.default()
 intents.message_content = True
 intents.presences = True
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
